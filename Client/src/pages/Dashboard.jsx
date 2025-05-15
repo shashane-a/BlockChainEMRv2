0f1b2ed6ef3d46d577066633bdf07aa4bee227d2
ec19389 <ec19389@qmul.ac.uk>
2025-05-15 08:55:08 +0100

    Fixed patient dashboard

--- Client/src/pages/Dashboard.jsx
+++ Client/src/pages/Dashboard.jsx
@@ -1,11 +1,15 @@
 import { usePatientData } from "../context/PatientDataContext";
 import { useAuth } from "../context/AuthContext.jsx";
 import { Link, useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 import axios from "axios";
 import AddProviderProfile from "../components/AddProviderProfile.jsx";
 import { toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import { contractABI, contractAddress } from "../contracts/PatientRegistryContract.js";
+import { ethers } from "ethers";
+import { fetchAndDecryptPatient } from "../utils/patients.js";
+
 
 export default function Dashboard() {
   const { auth } = useAuth();
@@ -14,6 +18,7 @@ export default function Dashboard() {
   const [ providers, setProviders ] = useState([])
   const [events, setEvents] = useState([]);
   const [providerProfile, setProviderProfile] = useState({});
+
   
 
 useEffect(() => {
@@ -32,7 +37,9 @@ useEffect(() => {
     }
   };
 
-  fetchProfile();
+  if (auth.role === "provider" || auth.role === "admin") {
+    fetchProfile();
+  }
   
   console.log("Provider profile:", providerProfile);
   //fetch profile data from the server
@@ -71,6 +78,51 @@ useEffect(() => {
     return response.data;
 
   }
+
+const loadPatient = async () => {
+  const encryptedPatient = await fetchAndDecryptPatient(auth.walletid);
+  console.log(encryptedPatient);
+  setPatients([encryptedPatient]);
+}
+
+useEffect(() => {
+  if (auth.role === "patient") {
+    loadPatient();
+  }
+}, [auth.walletid])
+    
+
+useEffect(() => {
+  async function getProvidersForPatient(){
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    const contract = new ethers.Contract(contractAddress, contractABI, provider);
+
+    const providers = await getProviders()
+    console.log("Providers data from patient:", providers); 
+
+    //check if each provider has access to the patient
+    const accessibleProviders = [];
+
+    for (const provider of providers) {
+      const hasAccess = await contract.canProviderAccess(patients[0].wallet_address, provider.wallet_address);
+      console.log("Access status for provider:", provider.wallet_address, ":", hasAccess);
+      if (hasAccess) {
+        accessibleProviders.push(provider);
+      }
+    }
+
+
+    console.log("Accessible providers:", accessibleProviders);
+    setProviders(accessibleProviders);
+
+  }
+
+  if (auth.role === "patient") {
+    getProvidersForPatient()
+  }
+
+}, [patients])
+
 
 
   return (
@@ -256,7 +308,6 @@ useEffect(() => {
           </div>
         </div>
 
-        {/* Right Column: Appointments + Prescriptions */}
         <div className="flex flex-col gap-4 flex-1 overflow-hidden">
           {/* Appointments */}
           <div className="p-4 rounded shadow-sm bg-white flex-1 flex flex-col overflow-hidden">
@@ -299,7 +350,7 @@ useEffect(() => {
           </div>
 
 
-          <div className="p-4 rounded shadow-sm bg-white flex-1 flex flex-col">
+          <div className="p-4 rounded shadow-sm bg-white flex-1 flex flex-col overflow-hidden">
             <div className="flex flex-row justify-between">
               <h2 className="text-2xl font-bold mb-4 text-[#112D4E]">Upcoming Appointments</h2>
               <Link 
@@ -308,35 +359,178 @@ useEffect(() => {
                 >
                 View all
               </Link>
-            </div>
-            {patients.map((patient, index) => (
-                <div key={index} className="p-4 my-2 rounded shadow-md bg-[#edeff1] flex flex-row justify-between">
-                  <div>
-                    <p><strong>Name:</strong> {patient.first_name} {patient.last_name}</p>
-                    <p><strong>Date of Birth:</strong> {patient.date_of_birth}</p>
-                    <p><strong>Gender:</strong> {patient.gender}</p>
-                    <p><strong>Added Date: </strong>28/04/2025 10:33</p>
-                  </div>
-                  
-                  <Link 
-                    to={`/patients/${patient.wallet_address}`} 
-                    className="flex gap-2 self-start py-2 px-2 rounded bg-[#3F72AF] text-white font-semibold text-sm cursor-pointer"
-                  >
-                    View
-                  </Link>
+            </div> 
+            <div className="flex-1 overflow-y-auto h-full">
+              {patients.map((patient, index) => (
+                <div key={index} className="my-4">
+
+                  {patient.appointments && patient.appointments.length > 0 ? (
+                    patient.appointments.map((appointment, aIndex) => (
+                      <div
+                        key={aIndex}
+                        className="p-4 my-2 rounded shadow-md bg-[#edeff1] flex flex-row justify-between"
+                      >
+                        <div>
+                          <p className="text-xl text-[#112D4E]"><strong>{appointment.title}</strong></p>
+                          <p><strong>Patient Name:</strong> {patient.first_name} {patient.last_name}</p>
+                          <p><strong>Date:</strong> {appointment.date}</p>
+                          <p><strong>Time:</strong> {appointment.time}</p>
+                          <p><strong>Description:</strong> {appointment.description}</p>
+                        </div>
+
+                        <Link
+                          to={`/patients/${patient.wallet_address}`}
+                          className="flex gap-2 self-start py-2 px-2 rounded bg-[#3F72AF] text-white font-semibold text-sm cursor-pointer"
+                        >
+                          View Patient
+                        </Link>
+                      </div>
+                    ))
+                  ) : (
+                    <p className="text-gray-500">No appointments available.</p>
+                  )}
                 </div>
               ))}
-              {patients.length === 0 && (
-                <div className="flex-1 overflow-y-auto">
-                  <p className="text-gray-500">No patients added</p>
-                </div>
-              )}
-            
+            </div>
           </div>
         
         </div>
       </div>
       )}
+      { auth.role === "patient" && (
+      <div className="flex flex-row gap-4 flex-1 overflow-hidden">
+
+        <div className="flex flex-col gap-4 flex-1 overflow-hidden">
+
+          <div className="p-4 rounded shadow-md bg-white flex-shrink-0 flex flex-col">
+          
+            <div>
+              <h2 className="text-2xl font-bold mb-2 text-[#112D4E]">Welcome {` ${patients[0].first_name} ${patients[0].last_name}`}</h2>
+              <p className="text-[#6d6d6d] text-lg font-semibold">Date Of birth: {patients[0].date_of_birth}</p>
+              <p className="text-gray-500 text-lg font-semibold">Gender: {patients[0].gender}</p>
+              <p className="text-gray-500 ">Wallet Address: {patients[0].wallet_address}</p>
+              <p className="text-gray-500">Created Date: {patients[0].created_date}</p>
+            </div>
+            <Link 
+              className="flex gap-2 self-start py-2 px-2 rounded bg-[#3F72AF] text-white font-semibold text-sm cursor-pointer mt-4"
+              to="/profile"
+            >
+              View Profile
+            </Link>
+            
+          </div>
+        
+
+          <div className="p-4 rounded shadow-sm bg-white flex-1 flex flex-col overflow-hidden">
+            <div className="flex flex-row justify-between">
+              <h2 className="text-2xl font-bold mb-4 text-[#112D4E]">Recent Activity</h2>
+              
+            </div>
+            <div>
+              {events.length > 0 ? (
+                <div className="flex flex-col gap-2">
+                  {events.map((event, index) => (
+                    <div key={index} className="p-4 my-2 rounded shadow-md bg-[#edeff1] flex flex-row justify-between">
+                      <div>
+                        <p><strong>Type:</strong> {event.type}</p>
+                        <p><strong>Date:</strong> {event.date}</p>
+                        <p><strong>Description:</strong> {event.description}</p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              ) : (
+                <div className="flex-1 overflow-y-auto">
+                  <p className="text-gray-500">No recent activity</p>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-4 flex-1 overflow-hidden">
+          <div className="p-4 rounded shadow-sm bg-white flex-1 flex flex-col overflow-hidden">
+            <div className="flex flex-row justify-between">
+              <h2 className="text-2xl font-bold mb-4 text-[#112D4E]">Upcoming Appointments</h2>
+              <Link 
+                className="flex gap-2 self-start py-2 px-2 rounded bg-[#3F72AF] text-white font-semibold text-sm cursor-pointer"
+                to="/appointments"
+                >
+                View all
+              </Link>
+            </div> 
+            <div className="flex-1 overflow-y-auto h-full">
+              {patients.map((patient, index) => (
+                <div key={index} className="my-4">
+
+                  {patient.appointments && patient.appointments.length > 0 ? (
+                    patient.appointments.map((appointment, aIndex) => (
+                      <div
+                        key={aIndex}
+                        className="p-4 my-2 rounded shadow-md bg-[#edeff1] flex flex-row justify-between"
+                      >
+                        <div>
+                          <p className="text-xl text-[#112D4E]"><strong>{appointment.title}</strong></p>
+                          <p><strong>Date:</strong> {appointment.date}</p>
+                          <p><strong>Time:</strong> {appointment.time}</p>
+                          <p><strong>Description:</strong> {appointment.description}</p>
+                        </div>
+
+                        <Link
+                          to={`/appointments`}
+                          className="flex gap-2 self-start py-2 px-2 rounded bg-[#3F72AF] text-white font-semibold text-sm cursor-pointer"
+                        >
+                          View Calender
+                        </Link>
+                      </div>
+                    ))
+                  ) : (
+                    <p className="text-gray-500">No appointments available.</p>
+                  )}
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="p-4 rounded shadow-sm bg-white flex-1 flex flex-col overflow-hidden">
+            <div className="flex flex-row justify-between">
+              <h2 className="text-2xl font-bold mb-4 text-[#112D4E]">Recenly Added Providers</h2>
+              <div className="flex gap-2">
+              <button 
+                className="flex gap-2 self-start py-2 px-2 rounded bg-[#3F72AF] text-white font-semibold text-sm cursor-pointer">
+                View all
+              </button>
+              </div>
+            </div>
+            <div className="flex-1 overflow-y-auto h-full">
+              {providers.length > 0 ? (
+                <div className="flex flex-col gap-2">
+                  {providers.map((provider, index) => (
+                    <div key={index} className="p-4 my-2 rounded shadow-md bg-[#edeff1] flex flex-row justify-between">
+                      <div>
+                        <p><strong>Name:</strong> {provider?.first_name} {provider?.last_name}</p>
+                        <p><strong>Job Title:</strong> {provider?.job_title}</p>
+                        <p><strong>Organisation:</strong> {provider?.orgnisation_name}</p>
+                        <p><strong>Added Date: </strong>{provider?.date_joined ? new Date(provider.date_joined).toLocaleDateString() : ''}</p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              ) : (
+                <div className="flex-1 overflow-y-auto">
+                  <p className="text-gray-500">No providers added</p>
+                </div>
+              )}
+
+            </div>
+
+          </div>
+
+          
+        
+        </div>
+      </div>
+      )}
+      
     </div>
 
 
